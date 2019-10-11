375dedc470b33a5d09f5d503621240a64e4812a6
Andrey Gaisinskii <andygaisinskii@gmail.com>
2019-10-11 17:33:40 +0300

    some basic preps are done

---
+++ src/views/PageMain/PageMain.vue
@@ -0,0 +1,24 @@
+<template>
+  <base-page name="main">
+    <h1 class="hehe">
+      header is here main page
+    </h1>
+  </base-page>
+</template>
+
+<script>
+import BasePage from '@/components/BasePage/BasePage.vue';
+
+export default {
+  components: {
+    BasePage,
+  },
+};
+</script>
+
+<style lang="scss">
+.page--main {
+  .page {
+  }
+}
+</style>
